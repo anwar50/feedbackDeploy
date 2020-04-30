46cdd8ed7f6bdc3c39756568f5f6f5119ab8dabd
anwar <anwaruuo88@gmail.com>
2020-04-30 01:20:56 +0100

    final deploy bismillah

--- automated_feedback/settings.py
+++ automated_feedback/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'automated_feedback.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 SITE_ID = 1
--- automated_feedback/urls.py
+++ automated_feedback/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('api/', include('feedbackapp.api.urls'), name='api_url'),   
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
